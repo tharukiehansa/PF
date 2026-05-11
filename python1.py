18563f74ec29ab4fe8cdd09f0b94d3e99a62ef55
tharukiehansa <tharukiehansa1@gmail.com>
2026-05-11 17:22:06 +0530

    Add files via upload
    python Q 1

---
+++ python1.py
@@ -0,0 +1,9 @@
+print("tharuki ehansa")
+print("tissa central collage)
+
+"""
+this is a multi-line coment
+it can be written in multipel line
+"""
+print("comments are used to explain the cord and make it easier to understandad"
+      
